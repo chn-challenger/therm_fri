7a00ae8a0b08dc38b6d81b78842ea46c9e7267ca
danielstpaul <danielstpaul@hotmail.com>
2015-10-01 15:17:07 +0100

    added weather API

--- app/public/application.js
+++ app/public/application.js
@@ -1,6 +1,9 @@
 console.log('Hello!');
 
 var thermostat = new Thermostat();
+var gpsurl = "http://api.wunderground.com/api/549637f16227567b/geolookup/conditions/q/";
+var gpslat = "UK";
+var gpslon = "/" + "London" + ".json";
 
 $(document).ready(function() {
 
@@ -30,10 +33,18 @@ $(document).ready(function() {
       thermostat.powerSavingOn();
     } else {
       thermostat.powerSavingOff();
-
     }
-
     $('#temperature_display').text(thermostat.temperature);
     $('#temperature_display').css('color', thermostat.colour);
+  });
+
+  $.ajax({
+  url : gpsurl + gpslat + gpslon,
+  dataType : "jsonp",
+  success : function(parsed_json) {
+  var location = parsed_json['location']['city'];
+  var temp_c = parsed_json['current_observation']['temp_c'];
+  $('#weather_display').text("Current temperature in " + location + " is: " + temp_c + "℃");
+  }
   });
 });
