980fdd97b2ca6fe44f38c3596f2f812d13a187da
danielstpaul <danielstpaul@hotmail.com>
2015-10-01 18:09:48 +0100

    Added Forecast

--- app/public/weather.js
+++ app/public/weather.js
@@ -9,11 +9,28 @@ $(document).ready(function() {
     success : function(parsed_json) {
       var location = parsed_json['location']['city'];
       var temp_c = parsed_json['current_observation']['temp_c'];
-      $('#weather_display').text("Current temperature in " + location + " is: " + temp_c + "℃");
       console.log(parsed_json);
-      var forecast = parsed_json['forecast']['txt_forecast']['forecastday'][1]['fcttext_metric'];
-      $('#forecast').text("Tomorrow: " + forecast);
+      $('#weather_display').text("Current temperature in " + location + " is: " + temp_c + "℃");
 
+      var forecast_image = parsed_json['current_observation']['icon_url'];
+      $('#forecast_image').html(function() {
+        return '<img src=' + forecast_image + '>'; });
+
+      var forecast1 = parsed_json['forecast']['txt_forecast']['forecastday'][2]['fcttext_metric'];
+      var forecast_day1 = parsed_json['forecast']['txt_forecast']['forecastday'][2]['title'];
+      $('#forecast1').text(forecast_day1 + ': ' + forecast1);
+
+      var forecast_image1 = parsed_json['forecast']['txt_forecast']['forecastday'][2]['icon_url'];
+      $('#forecast_image1').html(function() {
+        return '<img src=' + forecast_image1 + '>'; });
+
+      var forecast2 = parsed_json['forecast']['txt_forecast']['forecastday'][4]['fcttext_metric'];
+      var forecast_day2 = parsed_json['forecast']['txt_forecast']['forecastday'][4]['title'];
+      $('#forecast2').text(forecast_day2 + ': ' + forecast2);
+
+      var forecast_image2 = parsed_json['forecast']['txt_forecast']['forecastday'][4]['icon_url'];
+      $('#forecast_image2').html(function() {
+        return '<img src=' + forecast_image2 + '>'; });
     }
 
   });
